ed771ed44f3024360658c3e5f697ef2e8209aee4
yash <officialyashdobriyal@gmail.com>
2023-09-16 21:47:00 +0530

    login feature

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
-from wtforms.validators import DataRequired, Length,Email,EqualTo
+from wtforms.validators import DataRequired, Length,Email,EqualTo,ValidationError
+from flask_blog.models import User
 
 
 
@@ -11,9 +12,17 @@ class RegistrationForm(FlaskForm):
      password=PasswordField('Password',validators=[DataRequired(),Length(min=6)])
      confirm_password=PasswordField('Confirm Password',validators=[DataRequired(),Length(min=6), EqualTo('password')])
      submit= SubmitField('Sign Up')
+     def validate_username(self, username):
+          user=User.query.filter_by(username=username.data).first()
+          if user:
+               raise ValidationError('Username is taken')
     
+     def validate_email(self, email):
+               user=User.query.filter_by(email=email.data).first()
+               if user:
+                    raise ValidationError('Email already Exist')
     
-    
+
  
 class LoginForm(FlaskForm):
      email=StringField('Email', validators=[DataRequired(),Email()])
--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,6 +1,7 @@
-from flask import  render_template,url_for,flash,redirect
+from flask import  render_template,url_for,flash,redirect,request
 from flask_blog.forms import RegistrationForm, LoginForm
-from flask_blog import app
+from flask_blog import app,db,bcrypt
+from flask_login import login_user, current_user,logout_user,login_required
 from flask_blog.models import User,Post
 
 posts=[
@@ -29,24 +30,53 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+
+
+
 @app.route('/register',methods=["GET","POST"])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form=RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account Created For {form.username.data}!','success')
-        return redirect(url_for('home'))
+        hashed_password=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        # pushing data in data base and hashing the password
+        user=User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Account Created Succesfully ','success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Sign-Up',form=form)
+
+
+
 
 
 @app.route('/login',methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form=LoginForm()
     if form.validate_on_submit():
-    # down code was used as tmp database;
-        if form.email.data== 'admin@gmail.com' and form.password.data=='123456':
-         flash('You have logged in !!!!!!!!','success')
-         return redirect(url_for('home'))
+        user=User.query.filter_by(email=form.email.data).first()   
+        if user and bcrypt.check_password_hash(user.password,form.password.data):
+            login_user(user,remember=form.remember.data)
+            # if we are log out it will take us to the login page and then if we log in then we will be redirexted to 
+            # account page insted of home page 
+            next_page=request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash('Log in unsuccesfull, check username and password','danger')
+            flash('Log in unsuccesfull, check Email and Password','danger')
     return render_template('login.html', title='Login',form=form)
 
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/account')
+@login_required 
+def account():
+    imagefile=url_for('static',filename='profile_pics/'+current_user.image_file)
+    return render_template('account.html', title='Account',image_file=imagefile)
